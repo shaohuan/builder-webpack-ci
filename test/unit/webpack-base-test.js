0ee8ec31a9627a0bee13edaae7b3395f932543a4
shaohuan <shaohuan1985@126.com>
2019-11-22 08:56:22 +0800

    modify test case version IV

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -4,8 +4,9 @@ const assert = require('assert');
 describe('webpack.base.js test case', () => {
   const baseConfig = require('../../lib/webpack.base.js');
   // console.log(baseConfig);
-
+  console.log('entry files are ' + baseConfig.entry);
   it('entry', () => {
+
     assert.equal(baseConfig.entry.index.indexOf('builder-webpack/test/smoke/template/src/index/index.js') > -1, true);
     assert.equal(baseConfig.entry.search.indexOf('builder-webpack/test/smoke/template/src/search/index.js') > -1, true);
   })
